79d0e68c013e50d6fc3a00234a2fb051bf95d82a
Saynka <sankakale@gmail.com>
2020-10-03 17:44:13 -0700

    noideawhyitsworking

--- js/app.js
+++ js/app.js
@@ -5,9 +5,10 @@ $().ready(() => {
   $.ajax('data/page-1.json', { method: 'GET', dataType: 'JSON' }).then((data) => {
     data.forEach(hornedAnimal => {
       var newAnimal = new Animal(hornedAnimal);
-      newAnimal.createRender();
+      newAnimal.createHTML();
 
     });
+
   });
   function Animal(object) {
     this.title = object.title;
@@ -17,12 +18,31 @@ $().ready(() => {
     this.image_url = object.image_url;
     animalsArray.push(this);
   }
-  Animal.prototype.createRender = function () {
-    const $templateClone = $('#photo-template').clone();
-    $templateClone.css('display', 'block')
-    $templateClone.find('h2').text(this.title);
-    $templateClone.find('img').attr('src', this.image_url);
-    $templateClone.find('p').text(this.description);
-    $('main').append($templateClone)
+  // Animal.prototype.createRender = function () {
+  //   const $templateClone = $('#photo-template').clone();
+  //   $templateClone.css('display', 'block');
+  //   $templateClone.find('section').attr('class', this.keyword);
+  //   $templateClone.find('h2').text(this.title);
+  //   $templateClone.find('img').attr('src', this.image_url);
+  //   $templateClone.find('p').text(this.description);
+  //   $('main').append($templateClone)
+  // };
+
+  // function renderHello() {
+  //   // fetch('template.mustache')
+  //   //   .then((response) => response.text())
+  //   //   .then((template) => {
+  //       let template = $('template').html();
+  //       var rendered = Mustache.render(template, {});
+
+  //       document.getElementById('photo-template').innerHTML = rendered;
+  //     });
+  // }
+  Animal.prototype.createHTML = function () {
+    let myTemplate = $('#template').html();
+    let html = Mustache.render(myTemplate, this);
+
+    $('main').append(html);
   };
 });
+
