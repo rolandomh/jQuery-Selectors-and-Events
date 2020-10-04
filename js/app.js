44caf4b15161ba1a6b75a16f84871004eebbd2d5
Saynka <sankakale@gmail.com>
2020-10-04 15:57:13 -0700

    addedlab2stuff

--- js/app.js
+++ js/app.js
@@ -17,32 +17,38 @@ $().ready(() => {
     this.horns = object.horns;
     this.image_url = object.image_url;
     animalsArray.push(this);
-  }
-  // Animal.prototype.createRender = function () {
-  //   const $templateClone = $('#photo-template').clone();
-  //   $templateClone.css('display', 'block');
-  //   $templateClone.find('section').attr('class', this.keyword);
-  //   $templateClone.find('h2').text(this.title);
-  //   $templateClone.find('img').attr('src', this.image_url);
-  //   $templateClone.find('p').text(this.description);
-  //   $('main').append($templateClone)
-  // };
+  };
 
-  // function renderHello() {
-  //   // fetch('template.mustache')
-  //   //   .then((response) => response.text())
-  //   //   .then((template) => {
-  //       let template = $('template').html();
-  //       var rendered = Mustache.render(template, {});
 
-  //       document.getElementById('photo-template').innerHTML = rendered;
-  //     });
-  // }
   Animal.prototype.createHTML = function () {
     let myTemplate = $('#template').html();
     let html = Mustache.render(myTemplate, this);
 
     $('main').append(html);
   };
-});
 
+  function dropDown() {
+
+    let keywords = [];
+    //grab drop down ID
+
+    animalsArray.forEach((Animal) => {
+      let nameOfKey = Animal.keyword;
+      // ! - does the opposite
+      if (!keywords.includes(nameOfKey)) {
+        keywords.push(nameOfKey);
+      }
+    })
+
+    // add event listener to dropdown
+    $('#dropDown').on('change', function () {
+      $('.animal').remove();
+      animalsArray.forEach(Animal => {
+        if (Animal.keyword === this.value) {
+          Animal.template();
+        }
+      })
+    })
+  }
+
+});
